c429b282a622ff43e9f045cd3e0275e77cc1d176
Emilio Peláez <me@epelaez.net>
2021-11-05 20:34:10 +0100

    Create project

---
+++ SwiftUIJam21/SwiftUIJam21/SwiftUIJam21App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJam21App.swift
+//  SwiftUIJam21
+//
+//  Created by Emilio Peláez on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJam21App: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
