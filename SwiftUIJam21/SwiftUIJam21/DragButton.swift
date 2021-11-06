b03e543ea2144cae1c07306bbdb16b5f2a0b7b69
Emilio Peláez <me@epelaez.net>
2021-11-06 19:37:45 +0100

    Drag thumb

--- SwiftUIJam21/SwiftUIJam21/DragButton.swift
+++ SwiftUIJam21/SwiftUIJam21/DragButton.swift
@@ -13,26 +13,42 @@ struct DragButton: View {
 	let notches: Int = 10
 	
 	@State var expanded = false
+	@State var offset: Double = 0
 	
 	var body: some View {
-		ZStack {
-			Circle()
-				.fill(Color(.systemGray6))
-				.shadow(color: Color(white: 0, opacity: 0.25), radius: 3, x: 0, y: 3)
-			Circle()
-				.stroke(Color(.systemGray5))
-			Image(systemName: "plus")
-				.foregroundColor(.primary)
+		Group {
+			ZStack {
+				Circle()
+					.fill(Color(.systemGray6))
+					.shadow(color: Color(white: 0, opacity: 0.25), radius: 3, x: 0, y: 3)
+				Circle()
+					.stroke(Color(.systemGray5))
+				Image(systemName: "plus")
+					.foregroundColor(.primary)
+			}
+			.offset(x: 0, y: offset)
 		}
 		.frame(width: thumbSize, height: thumbSize)
-		.onTapGesture {
-			withAnimation(.easeIn(duration: 0.2)) {
-				expanded.toggle()
-			}
-		}
 		.background(alignment: .bottom) {
 			Track(thumbSize: thumbSize, expandedHeight: expandedHeight, notches: notches, expanded: expanded)
 		}
+		.gesture(drag)
+	}
+	
+	var drag: some Gesture {
+		DragGesture()
+			.onChanged { value in
+				if !expanded {
+					return withAnimation { expanded = true }
+				}
+				offset = min(0, max(-expandedHeight + thumbSize, value.translation.height))
+			}
+			.onEnded { _ in
+				withAnimation {
+					expanded = false
+					offset = 0
+				}
+			}
 	}
 }
 
